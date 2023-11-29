2b10c263b248aad064640cd89dde35fc6d6ddd17
PraneshBK <44346322+PraneshBalasubramaniam@users.noreply.github.com>
2023-11-29 17:39:20 +0000

    triggering code dimming on extension start, command trigger, and editor change

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,34 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+function dimFile(activeEditor: vscode.TextEditor) {
+	if (!activeEditor.document.getText().trim()) { return; }
+	const position = activeEditor.selection.active;
+	console.log(position);
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "block-dimmer" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('block-dimmer.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from block dimmer!');
-	});
-
-	context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+export function activate(context: vscode.ExtensionContext) {
+	console.log('extension active');
+	let disposable = vscode.commands.registerCommand('block-dimmer.helloWorld', () => {
+		const activeEditor = vscode.window.activeTextEditor;
+		if (!activeEditor) { return; }
+		console.log('trigger');
+		dimFile(activeEditor);
+	});
+	context.subscriptions.push(disposable);
+	const activeEditor = vscode.window.activeTextEditor;
+	if (!activeEditor) { return; }
+	console.log('start');
+	dimFile(activeEditor);
+}
+
+vscode.window.onDidChangeActiveTextEditor((activeEditor) => {
+	if (!activeEditor) { return; }
+	const fileExtName = activeEditor.document.fileName.split('.').pop();
+	if (!fileExtName || !['js', 'jsx', 'ts', 'tsx'].includes(fileExtName)) { return; }
+	console.log('onchange');
+	dimFile(activeEditor);
+});
+
+
+export function deactivate() { }
